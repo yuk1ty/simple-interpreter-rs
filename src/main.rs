ab8c0b677adaf930099e8381a68402cee1c3200f
Yuki <yuki.mul.tiplus@gmail.com>
2018-07-28 11:30:49 +0900

    Add variable expr

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 use std::fmt;
 use std::cell::RefCell;
+use std::collections::HashMap;
 
+#[allow(dead_code)]
 pub struct Machine {
-    pub expression: RefCell<Token>,
+    expression: RefCell<Token>,
+    environment: RefCell<HashMap<String, Token>>,
 }
 
 impl Machine {
     pub fn new(expression: Token) -> Self {
         Machine {
             expression: RefCell::new(expression),
+            environment: RefCell::new(HashMap::new()),
         }
     }
 
     pub fn run(&self) {
+        let environment: RefCell<HashMap<String, Token>> = RefCell::new(HashMap::new());
+
         while self.expression.borrow().is_reducible() {
             println!("{}", self.expression.borrow());
-            self.step();
+            self.step(&environment);
         }
 
         println!("{}", self.expression.borrow());
     }
 
-    fn step(&self) {
+    fn step(&self, environment: &RefCell<HashMap<String, Token>>) {
         self.expression
-            .replace(self.expression.clone().into_inner().reduce());
+            .replace(self.expression.clone().into_inner().reduce(environment));
     }
 }
 
@@ -31,6 +37,7 @@ impl Machine {
 pub enum Token {
     Number(i32),
     BoolValue(bool),
+    Var(String),
     Add(Box<Token>, Box<Token>),
     Multiply(Box<Token>, Box<Token>),
     LessThan(Box<Token>, Box<Token>),
@@ -42,7 +49,8 @@ impl fmt::Display for Token {
         // TODO clean up
         match self {
             &Number(v) => write!(f, "{}", v),
-            &BoolValue(ref b) => write!(f, "{}", b),
+            &BoolValue(b) => write!(f, "{}", b),
+            &Var(ref n) => write!(f, "{}", n),
             &Add(ref blv, ref brv) => write!(f, "{} + {}", blv.to_string(), brv.to_string()),
             &Multiply(ref blv, ref brv) => write!(f, "{} * {}", blv.to_string(), brv.to_string()),
             &LessThan(ref blv, ref brv) => write!(f, "{} < {}", blv.to_string(), brv.to_string()),
@@ -56,22 +64,27 @@ impl Token {
         match *self {
             Number(_) => false,
             BoolValue(_) => false,
+            Var(_) => true,
             Add(_, _) => true,
             Multiply(_, _) => true,
             LessThan(_, _) => true,
         }
     }
 
-    pub fn reduce(&self) -> Token {
+    pub fn reduce(&self, env: &RefCell<HashMap<String, Token>>) -> Token {
         use Token::*;
         match self {
             &Number(_) => panic!("Number token couldn't reduce!"),
             &BoolValue(_) => panic!("BoolValue token couldn't reduce!"),
+            &Var(ref name) => env.borrow()
+                .get(name)
+                .expect("Variable couldn't get!")
+                .clone(),
             &Add(ref blv, ref brv) if blv.is_reducible() => {
-                Add(Box::new(blv.reduce()), brv.clone())
+                Add(Box::new(blv.reduce(env)), brv.clone())
             }
             &Add(ref blv, ref brv) if brv.is_reducible() => {
-                Add(blv.clone(), Box::new(brv.reduce()))
+                Add(blv.clone(), Box::new(brv.reduce(env)))
             }
             &Add(ref blv, ref brv) => match **blv {
                 Number(left_value) => match **brv {
@@ -81,10 +94,10 @@ impl Token {
                 _ => panic!("Unexpected error in Add!"),
             },
             &Multiply(ref blv, ref brv) if blv.is_reducible() => {
-                Multiply(Box::new(blv.reduce()), brv.clone())
+                Multiply(Box::new(blv.reduce(env)), brv.clone())
             }
             &Multiply(ref blv, ref brv) if brv.is_reducible() => {
-                Multiply(blv.clone(), Box::new(brv.reduce()))
+                Multiply(blv.clone(), Box::new(brv.reduce(env)))
             }
             &Multiply(ref blv, ref brv) => match **blv {
                 Number(left_value) => match **brv {
@@ -94,10 +107,10 @@ impl Token {
                 _ => panic!("Unexpected error in Multiply!"),
             },
             &LessThan(ref blv, ref brv) if blv.is_reducible() => {
-                LessThan(Box::new(blv.reduce()), brv.clone())
+                LessThan(Box::new(blv.reduce(env)), brv.clone())
             }
             &LessThan(ref blv, ref brv) if brv.is_reducible() => {
-                LessThan(blv.clone(), Box::new(brv.reduce()))
+                LessThan(blv.clone(), Box::new(brv.reduce(env)))
             }
             &LessThan(ref blv, ref brv) => match **blv {
                 Number(left_value) => match **brv {
@@ -165,9 +178,11 @@ fn test_reduce() {
             Box::new(Multiply(Box::new(Number(3)), Box::new(Number(4)))),
         );
 
-        let reduced = expression.reduce();
-        let reduced2 = reduced.reduce();
-        let reduced3 = reduced2.reduce();
+        let _env: RefCell<HashMap<String, Token>> = RefCell::new(HashMap::new());
+
+        let reduced = expression.reduce(&_env);
+        let reduced2 = reduced.reduce(&_env);
+        let reduced3 = reduced2.reduce(&_env);
 
         assert_eq!(reduced.to_string(), "2 + 3 * 4");
         assert_eq!(reduced2.to_string(), "2 + 12");
@@ -180,11 +195,33 @@ fn test_reduce() {
             Box::new(Add(Box::new(Number(2)), Box::new(Number(2)))),
         );
 
-        let reduced = expression.reduce();
-        let reduced2 = reduced.reduce();
+        let _env: RefCell<HashMap<String, Token>> = RefCell::new(HashMap::new());
+
+        let reduced = expression.reduce(&_env);
+        let reduced2 = reduced.reduce(&_env);
 
         assert_eq!(reduced.to_string(), "5 < 4");
         assert_eq!(reduced2.to_string(), "false");
+    }
+
+    {
+        let expression = Add(
+            Box::new(Var("x".to_string())),
+            Box::new(Var("y".to_string())),
+        );
+
+        let env = RefCell::new(HashMap::new());
+        env.borrow_mut().insert("x".to_string(), Number(3));
+        env.borrow_mut().insert("y".to_string(), Number(4));
+
+        let reduced = expression.reduce(&env);
+        let reduced2 = reduced.reduce(&env);
+        let reduced3 = reduced2.reduce(&env);
+
+        assert_eq!(expression.to_string(), "x + y");
+        assert_eq!(reduced.to_string(), "3 + y");
+        assert_eq!(reduced2.to_string(), "3 + 4");
+        assert_eq!(reduced3.to_string(), "7");
     }
 }
 
